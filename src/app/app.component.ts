dd71309c7b147b07219a30c1393fd291811dec61
Alan Christian Ramírez Rodríguez <ingenieroacramirez@gmail.com>
2025-07-10 12:09:35 -0600

    refactor: clean up imports in app component
    - Removed IonRouterOutlet from imports in app.component.ts as it is no longer needed.
    - Streamlined the import statements for better readability and maintainability.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
-import {
-  IonApp,
-  IonContent,
-  IonRouterOutlet,
-  IonButton,
-} from '@ionic/angular/standalone';
+import { IonApp, IonContent, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonContent, IonRouterOutlet, IonButton],
+  imports: [IonApp, IonContent, IonButton],
 })
 export class AppComponent {
   constructor() {}
